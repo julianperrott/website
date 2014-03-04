181affd866eb1b8d29f96288e87cf8fa1ce88ecf
julianperrott <julian_perrott@hotmail.com>
2014-03-04 21:56:33 +0000

    Bundled JS and CSS

--- JulianPerrottName/App_Start/BundleConfig.cs
+++ JulianPerrottName/App_Start/BundleConfig.cs
@@ -7,6 +7,25 @@
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/Areas/Home/js/bundle").Include(
+                "~/Areas/Home/js/bootstrap.js",
+                "~/Areas/Home/js/jquery-1.9.1.js",
+                "~/Areas/Home/js/custom.js",
+                "~/Areas/Home/js/scrolltopcontrol.js",
+                "~/Areas/Home/js/jquery-ui-1.10.4.custom.js",
+                "~/Areas/Home/js/jquery.youtubepopup.js"));
+
+            bundles.Add(new StyleBundle("~/Areas/Home/css/bundle").Include(
+                  "~/Areas/Home/css/bootstrap.css",
+                  "~/Areas/Home/css/lato.css",
+                  "~/Areas/Home/css/animate.css",
+                  "~/Areas/Home/css/elements.css",
+                  "~/Areas/Home/css/jquery-ui-1.10.4.custom.css",
+                  "~/Areas/Home/css/font-awesome.css",
+                  "~/Areas/Home/css/custom.css"));
+
+          //  // Code removed for clarity.
+          BundleTable.EnableOptimizations = true;
         }
     }
 }
